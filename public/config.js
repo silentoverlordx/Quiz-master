1f81c3ef38415f7eae3f0a5c0691d658ccfdc49a
silentoverlordx <silentoverlordx@gmail.com>
2025-09-05 14:17:46 +0530

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,7 @@
+// config.js
+const SUPABASE_URL = "https://kocahzranmsnegjtvqro.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvY2FoenJhbm1zbmVnanR2cXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MDg5ODIsImV4cCI6MjA3MjQ4NDk4Mn0.AhJ-mcgh08q14ScOJGWDozeWxqMJknjc2WMdYzyAH9s";
+
+// make sure they're available globally
+window.SUPABASE_URL = SUPABASE_URL;
+window.SUPABASE_KEY = SUPABASE_KEY;
